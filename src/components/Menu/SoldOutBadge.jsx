63c84e003d3b87c89a9893e9633666862ca59959
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-11-11 21:07:17 -0800

    SoldOutBadge created

---
+++ src/components/Menu/SoldOutBadge.jsx
@@ -0,0 +1,7 @@
+export default function SoldOutBadge() {
+  return (
+    <span className="absolute top-2 left-2 bg-red-600 text-white text-xs font-bold px-2 py-1 rounded">
+      SOLD OUT
+    </span>
+  );
+}
