d98fe73b2c21d7fe5875e264fa65cd5d8e8f34a2
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-11-13 17:53:33 -0800

    subCategoryTab's style updated

--- src/components/Menu/CategoryTabs.jsx
+++ src/components/Menu/CategoryTabs.jsx
@@ -1,6 +1,6 @@
 export default function CategoryTabs({ categories, selected, onSelect }) {
   return (
-    <div className="flex gap-2 overflow-x-auto py-2 mb-4 text-center border-t border-b sm:justify-center">
+    <div className="flex gap-2 overflow-x-auto py-2 mb-2 text-center border-t border-b sm:justify-center">
       {categories.map((category) => (
         <button
           key={category}
