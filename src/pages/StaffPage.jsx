d5f8066e0a0b71d5ca09cfa85158981263b66c8f
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-11-13 21:22:59 -0800

    StaffPage.jsx's page scafold is created

--- src/pages/StaffPage.jsx
+++ src/pages/StaffPage.jsx
@@ -1,3 +1,22 @@
 export default function StaffPage() {
-  return <div>StaffPage</div>;
+  return (
+    <div className="min-h-screen p-2 bg-gray-100">
+      <h1 className="text-2xl sm:text-3xl font-bold mb-4">Menu Editor</h1>
+
+      {/* Add new item button */}
+      <button className="mb-4 bg-blue-600 text-white px-4 py-2 rounded-lg">
+        + Add New Menu
+      </button>
+
+      {/* Form modal */}
+      {/* <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4">
+        <div className="bg-white w-full max-w-md rounded-xl p-4">
+          Staff Menu Form
+        </div>
+      </div> */}
+
+      {/* Menu Items List */}
+      <div className="grid gap-4">staffitemcard</div>
+    </div>
+  );
 }
