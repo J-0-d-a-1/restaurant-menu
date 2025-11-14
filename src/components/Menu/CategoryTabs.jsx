b316240aa366766ec3c1e74629886e350cff8ca2
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-11-13 17:40:41 -0800

    categoryTab's style updated

--- src/components/Menu/CategoryTabs.jsx
+++ src/components/Menu/CategoryTabs.jsx
@@ -1,14 +1,12 @@
 export default function CategoryTabs({ categories, selected, onSelect }) {
   return (
-    <div className="flex gap-2 overflow-x-auto py-2 mb-4">
+    <div className="flex gap-2 overflow-x-auto py-2 mb-4 text-center border-t border-b sm:justify-center">
       {categories.map((category) => (
         <button
           key={category}
-          className={`flex-shrink-0 px-4 py-2 rounded-full text-sm font-medium ${
-            selected === category
-              ? "bg-blue-600 text-white"
-              : "bg-gray-200 text-gray-700"
-          } hover:bg-blue-500 hover:text-white transition`}
+          className={`flex-shrink-0 px-4 py-2 text-sm font-medium ${
+            selected === category ? "text-red-600 underline" : "text-gray-700"
+          }  hover:text-red-600 transition`}
           onClick={() => onSelect(category)}
         >
           {category}
