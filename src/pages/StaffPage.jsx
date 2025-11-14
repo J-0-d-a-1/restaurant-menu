cf93a263f6d54cde4783ba3e76481323a4cf7dd1
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-11-14 14:37:23 -0800

    passed down to StaffItemCard.jsx

--- src/pages/StaffPage.jsx
+++ src/pages/StaffPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import { menu } from "../data/menuData";
+import StaffItemCard from "../components/Staff/StaffItemCard";
 
 export default function StaffPage() {
   const [items, setItems] = useState(menu);
@@ -24,30 +25,7 @@ export default function StaffPage() {
       {/* Menu Items List */}
       <div className="grid gap-4 sm:grid-cols-3">
         {items.map((item) => (
-          <div className="bg-white rounded-lg p-4 shadow-sm border">
-            <img
-              src={item.images[0]}
-              alt={item.name}
-              className="w-full h-40 object-cover rounded-md mb-3"
-            />
-            <h2 className="font-bold text-lg">{item.name}</h2>
-            <p className="text-sm text-gray-600">{item.category}</p>
-
-            {item.subCategory && (
-              <p className="text-xs text-gray-500">{item.subCategory}</p>
-            )}
-
-            <p className="mt-2 font-semibold">${item.price}</p>
-
-            <div className="flex gap-2 mt-4">
-              <button className="flex-1 bg-green-600 text-white py-1 rounded ">
-                Edit
-              </button>
-              <button className="flex-1 bg-red-600 text-white py-1 rounded">
-                Delete
-              </button>
-            </div>
-          </div>
+          <StaffItemCard key={item.id} item={item} />
         ))}
       </div>
     </div>
