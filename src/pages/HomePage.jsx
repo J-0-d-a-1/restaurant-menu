7bb41accdfc5685f07beb0d0062b1c8708641543
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2026-01-30 14:20:30 -0800

    removed test h1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,9 +3,6 @@ import { Link } from "react-router-dom";
 export default function HomePage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-xs opacity-50">
-        DEPLOY CHECK: 2026-01-23-hashrouter
-      </h1>
       <div className="flex flex-col gap-4 w-64">
         <h1 className="text-2xl font-bold text-center mb-4">Welcom</h1>
 
