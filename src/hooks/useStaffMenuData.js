4d2d627f88d7c55237ced93738400c4617bb5faa
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2026-02-08 14:43:45 -0800

    useStaffMenuData created with initialState

---
+++ src/hooks/useStaffMenuData.js
@@ -0,0 +1,9 @@
+const initialState = {
+  items: [],
+  categories: [],
+  allSubCategories: [],
+  subCategories: [],
+  selectedCategory: null,
+  selectedSubCategory: null,
+  editingItem: null,
+};
