33eba46d0b66eb1ceddd364642e78968b078a5e7
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2026-02-13 17:11:37 -0800

    removed old code from MenuPage

--- src/pages/MenuPage.jsx
+++ src/pages/MenuPage.jsx
@@ -1,9 +1,3 @@
-// import { supabase } from "../lib/supabase";
-
-// import { useEffect, useState } from "react";
-
-// import { mapMenuFromDB } from "../utils/menuMapper";
-
 import { useMenuData } from "../hooks/useMenuData";
 
 import CategoryTabs from "../components/Menu/CategoryTabs";
@@ -12,83 +6,6 @@ import MenuGrid from "../components/Menu/MenuGrid";
 import MenuItemModal from "../components/Menu/MenuItemModal";
 
 export default function MenuPage() {
-  // const [menus, setMenus] = useState([]);
-  // const [categories, setCategories] = useState([]);
-  // const [selectedCategory, setSelectedCategory] = useState(null);
-  // const [subCategories, setSubCategories] = useState([]);
-  // const [selectedSubCategory, setSelectedSubCateopry] = useState(null);
-  // const [selectedItem, setSelectedItem] = useState(null);
-  // const [currentIndex, setCurrentIndex] = useState(0);
-
-  // // Read data from supabase
-  // useEffect(() => {
-  //   const fetchInitialData = async () => {
-  //     const [
-  //       { data: catData, error: catError },
-  //       { data: menuData, error: menuError },
-  //     ] = await Promise.all([
-  //       supabase.from("categories").select("*").order("sort_order"),
-  //       supabase
-  //         .from("menus")
-  //         .select("*")
-  //         .eq("hide", false)
-  //         .order("sort_order"),
-  //     ]);
-
-  //     if (catError || menuError) {
-  //       console.error(catError || menuError);
-  //       return;
-  //     }
-
-  //     setCategories(catData);
-  //     setSelectedCategory(catData[0]);
-  //     setMenus(menuData.map(mapMenuFromDB));
-  //   };
-
-  //   fetchInitialData();
-  // }, []);
-
-  // // fetching categories
-  // useEffect(() => {
-  //   if (categories.length && !selectedCategory) {
-  //     setSelectedCategory(categories[0]);
-  //   }
-  // }, [categories, selectedCategory]);
-
-  // // update subcategories when category changes
-  // useEffect(() => {
-  //   if (!selectedCategory) return;
-
-  //   const fetchSubCategories = async () => {
-  //     const { data, error } = await supabase
-  //       .from("subcategories")
-  //       .select("*")
-  //       .eq("category_id", selectedCategory.id)
-  //       .order("sort_order");
-
-  //     if (error) {
-  //       console.log(error);
-  //       return;
-  //     }
-
-  //     setSubCategories(data);
-  //     setSelectedSubCateopry(null);
-  //   };
-
-  //   fetchSubCategories();
-  // }, [selectedCategory]);
-
-  // // Filter menu items
-  // const filteredMenu = menus.filter((item) => {
-  //   const matchCategory = item.categoryId === selectedCategory?.id;
-
-  //   const matchSubCategory =
-  //     selectedSubCategory === null ||
-  //     item.subCategoryId === selectedSubCategory?.id;
-
-  //   return matchCategory && matchSubCategory;
-  // });
-
   const { state, dispatch, filteredMenu } = useMenuData();
 
   return (
