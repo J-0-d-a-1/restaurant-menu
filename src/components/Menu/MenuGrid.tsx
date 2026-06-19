81f1c6516729893d8c3f61a39b086e9e908618fa
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2026-06-19 16:47:05 -0700

    converted MenuGrid to tsx

--- src/components/Menu/MenuGrid.tsx
+++ src/components/Menu/MenuGrid.tsx
@@ -1,6 +1,12 @@
+import type { MenuItem } from "../../types";
 import MenuItemCard from "./MenuItemCard";
 
-export default function MenuGrid({ items, onItemClick }) {
+interface MenuGridProps {
+  items: MenuItem[];
+  onItemClick: (item: MenuItem) => void;
+}
+
+export default function MenuGrid({ items, onItemClick }: MenuGridProps) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
       {items.map((item) => (
